93424e3c6f293c519b32f1e1132cfb6beafc5920
David <david@outlier.org>
2019-06-14 12:26:08 -0700

    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const http = require('http')
+const http = require("http");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') return respondHello(req, res)
-  if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url === "/") return respondHello(req, res);
+  if (req.url === "/user-agent") return respondUserAgent(req, res);
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res);
 
-  res.end()
-})
+  res.end();
+});
 
-function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
+function respondHello(req, res) {
+  res.end(JSON.stringify({ msg: "hello" }));
 }
 
-function respondUserAgent (req, res) {
-  const ua = req.headers['user-agent']
-  res.end(JSON.stringify({ ua }))
+function respondUserAgent(req, res) {
+  const ua = req.headers["user-agent"];
+  res.end(JSON.stringify({ ua }));
+}
+function respondBase64(req, res) {
+  const phrase = req.url.replace(/^\/b64\//, "");
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
 }
 
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
 
-if (require.main !== module) module.exports = server
+if (require.main !== module) module.exports = server;
--- test.js
+++ test.js
@@ -1,31 +1,40 @@
-const tape = require('tape')
-const jsonist = require('jsonist')
+const tape = require("tape");
+const jsonist = require("jsonist");
 
-const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
-const server = require('./server')
+const PORT = (process.env.PORT =
+  process.env.PORT || require("get-PORT-sync")());
+const server = require("./server");
 
-const urlBase = `http://localhost:${PORT}`
+const urlBase = `http://localhost:${PORT}`;
 
-tape('should respond hello', (t) => {
+tape("should respond hello", (t) => {
   jsonist.get(urlBase, (err, body) => {
-    if (err) t.error(err)
+    if (err) t.error(err);
 
-    t.equal(body.msg, 'hello')
-    t.end()
-  })
-})
+    t.equal(body.msg, "hello");
+    t.end();
+  });
+});
 
-tape('should respond user-agent', (t) => {
-  const opts = { headers: { 'User-Agent': 'tape' } }
+tape("should respond user-agent", (t) => {
+  const opts = { headers: { "User-Agent": "tape" } };
   jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
-    if (err) t.error(err)
+    if (err) t.error(err);
 
-    t.equal(body.ua, 'tape')
-    t.end()
-  })
-})
+    t.equal(body.ua, "tape");
+    t.end();
+  });
+});
+tape("should respond b64", (t) => {
+  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+    if (err) t.error(err);
 
-tape('cleanup', function (t) {
-  server.close()
-  t.end()
-})
+    t.equal(body.b64, "aGVsbG8=");
+    t.end();
+  });
+});
+
+tape("cleanup", function (t) {
+  server.close();
+  t.end();
+});
